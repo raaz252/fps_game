7b48af5d23eb46a2863560c4bf66678365e085dd
51009920 <rajojha252@gmail.com>
2023-06-04 01:08:30 +0530

    Add project files.

---
+++ fps_game/fps2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+
+#include <stdio.h>
+#include <Windows.h>
+
+int nScreenWidth = 120;			// Console Screen Size X (columns)
+int nScreenHeight = 40;			// Console Screen Size Y (rows)
+int nMapWidth = 16;				// World Dimensions
+int nMapHeight = 16;
+
+float fPlayerX = 14.7f;			// Player Start Position
+float fPlayerY = 5.09f;
+float fPlayerA = 0.0f;			// Player Start Rotation
+float fFOV = 3.14159f / 4.0f;	// Field of View
+float fDepth = 16.0f;			// Maximum rendering distance
+float fSpeed = 5.0f;			// Walking Speed
